559b9470bd4fb5186d2a45234bf09b11cd17fe1d
Stefan Balke <stefan@balke.at>
2023-05-03 18:52:28 +0200

    use pymupdf

--- 02_extract_text.py
+++ 02_extract_text.py
@@ -4,6 +4,7 @@ import easyocr
 import cv2
 import numpy as np
 from pypdf import PdfReader
+import fitz
 
 # %%
 df_metadata = pd.read_csv('metadata.csv', header=0)
@@ -13,12 +14,21 @@ df_metadata = pd.read_csv('metadata.csv', header=0)
 ocr_reader = easyocr.Reader(['de', 'en'])
 
 # %%
+def pix2np(pix):
+    # Source: https://stackoverflow.com/a/53066662
+    im = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
+    im = np.ascontiguousarray(im[..., [2, 1, 0]])  # rgb to bgr
+    return im
+
+
 def process_image(path):
     # Read first page from PDF
-    reader = PdfReader(path)
-    scan = reader.pages[0].images[0].data
-    scan = np.frombuffer(scan, np.uint8)
-    scan = cv2.imdecode(scan, cv2.IMREAD_COLOR)
+    # reader = PdfReader(path)
+    # scan = reader.pages[0].images[0].data
+    scan = fitz.open(path)
+    scan = scan[0].get_pixmap(dpi=300)
+    scan = pix2np(scan)
+    # scan = cv2.imdecode(scan, cv2.IMREAD_COLOR)
     scan = cv2.cvtColor(scan, cv2.COLOR_BGR2RGB)
     scan = cv2.cvtColor(scan, cv2.COLOR_BGR2GRAY)
 
@@ -27,20 +37,18 @@ def process_image(path):
     # plt.show()
 
     # extract text
-    # result = ocr_reader.readtext(scan)
-    result = 0
+    result = ocr_reader.readtext(scan)
+
     return result
 
 # %%
 results = dict()
 
 for _, cur_row in df_metadata.head(10).iterrows():
-    cur_path = cur_row['path']
-    
+    cur_path = cur_row['path']  
 
     try:
         results[cur_path] = process_image(cur_path)
     except:
         print('Skipped {}'.format(cur_path))
 
-# %%
