8b2e24470270efc4a65c5504983adba2a7562d8b
Stefan Balke <stefan@balke.at>
2023-05-03 17:14:07 +0200

    add gitignore and stats, use pathlib instead of os

--- 01_extract-dataset.py
+++ 01_extract-dataset.py
@@ -1,34 +1,34 @@
 # %%
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
+from pypdf import PdfReader
 
 # %%
-my_dir = "/Users/stefan/Dropbox/MZE Noten"
-
-# %% [markdown]
-# # First Ideas
+path_data = Path('data') 
+# %%
+# Gather dataset
 # Iterate through the whole directory and save the filename and the first and second directory-instrumentname
 
-# %%
 df_metadata = []
 
-for dirpath, dirnames, filenames in os.walk(my_dir):
-    for filename in filenames:
-        if filename.endswith('.pdf'):
-            cur_file = dict()
-            cur_file['fn'] = filename
-            cur_file['instrument_group'] = dirpath.split(os.path.sep)[5]
+for cur_path in path_data.rglob('*.pdf'):
+    cur_file = dict()
+    cur_file['path'] = str(cur_path)
+    cur_file['fn'] = cur_path.name
+    cur_path_parts = cur_path.parts
+    cur_file['instrument_group'] = cur_path_parts[1]
 
-            try:
-                # if it has two levels of instruments
-                cur_file['instrument'] = dirpath.split(os.path.sep)[6]
-            except:
-                cur_file['instrument'] = ''
+    if len(cur_path_parts) >= 5:
+        cur_file['instrument_voice'] = cur_path_parts[2]
+    else:
+        cur_file['instrument_voice'] = ''
 
-            cur_file['path'] = os.path.join(cur_file['instrument_group'], cur_file['instrument'], cur_file['fn'])
+    # open pdf file
+    reader = PdfReader(cur_path)
+    cur_file['number_of_pages'] = len(reader.pages)
 
-            df_metadata.append(cur_file)
+    df_metadata.append(cur_file)
 
 df_metadata = pd.DataFrame(df_metadata)
 print(df_metadata.head(10))
@@ -51,16 +51,17 @@ def extract_title(filename):
 
 df_metadata['instrument_from_fn'] = df_metadata['fn'].apply(extract_instrument)
 df_metadata['title'] = df_metadata['fn'].apply(extract_title)
-df_metadata
 
 # %%
 # Data Cleanup
-df_metadata[df_metadata['instrument_group'] == 'Partitur']
 
-# partitur is not considered
+df_metadata = df_metadata.drop(index=df_metadata[df_metadata['instrument_group'] == 'Partitur'].index)
+df_metadata = df_metadata[~df_metadata['path'].str.contains('Marsch CD Walter Behrens')]
+df_metadata = df_metadata[~df_metadata['path'].str.contains('Bläserbuch zum Gotteslob')]
+df_metadata = df_metadata.drop_duplicates(subset=['title', 'fn'])
 
 # %%
-# First Statistics
+# Dataset Statistics
 
 print('number of files:', df_metadata.shape)
 
@@ -74,7 +75,21 @@ plt.show()
 df_metadata.groupby('instrument_group').size().sort_values(ascending=True).plot.barh()
 plt.show()
 
+# number of titles per instrument group
+stats_titles = (~df_metadata.groupby(['instrument_group', 'title']).size().unstack(0).isna()).sum()
+stats_titles.sort_values(ascending=True).plot.barh()
+plt.show()
+
 # number of files per instrument
 df_metadata.groupby('instrument_from_fn').size().sort_values(ascending=True).plot.barh(figsize=(10, 30), fontsize=4)
+plt.show()
+
+# number of pages
+df_metadata.groupby('number_of_pages').size().plot.barh()
+plt.show()
+
+df_metadata.boxplot(column='number_of_pages', by='instrument_group', rot=90)
+plt.show()
+
 
 # %%
