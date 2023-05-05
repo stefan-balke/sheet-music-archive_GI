2c01b57f1dc268ce3c0eb72c0282a95bb047dcc1
Stefan Balke <stefan@balke.at>
2023-05-05 09:54:20 +0200

    some seaborn plots

--- 01_extract-dataset.py
+++ 01_extract-dataset.py
@@ -1,8 +1,10 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from pathlib import Path
 from pypdf import PdfReader
+sns.set_theme(style='whitegrid')
 
 # %%
 path_data = Path('data') 
@@ -60,28 +62,63 @@ df_metadata = df_metadata[~df_metadata['path'].str.contains('Marsch CD Walter Be
 df_metadata = df_metadata[~df_metadata['path'].str.contains('Bläserbuch zum Gotteslob')]
 df_metadata = df_metadata.drop_duplicates(subset=['title', 'fn'])
 
+# %% save
+df_metadata.to_csv('metadata.csv')
+
 # %%
 # Dataset Statistics
+df_metadata = pd.read_csv('metadata.csv', header=0)
 
 print('number of files:', df_metadata.shape)
-
+print('number of pages:', df_metadata['number_of_pages'].sum())
 print('number of pieces:', df_metadata.groupby('title').ngroups)
 
-print('number of files per piece')
 df_metadata.groupby('title').size().sort_values(ascending=True).plot.barh(figsize=(10, 30), fontsize=4)
+plt.title('Number of Files per Piece')
 plt.show()
+
+df_metadata.groupby('title').size().sort_values(ascending=True).tail(20).plot.barh(figsize=(10, 10))
+plt.title('Number of Files per Piece (Top 20)')
+plt.savefig('number_of_files_per_piece_top-20.pdf')
+plt.show()
+
+df_plot = df_metadata.groupby('title').size().sort_values(ascending=True).tail(20)
+sns.set_color_codes('muted')
+sns.barplot(y=df_plot.index, x=df_plot.values, color='b')
+plt.xlabel('#Files per Musical Work')
+plt.ylabel('Musical Work')
+sns.despine(left=True, bottom=True)
+plt.savefig('number_of_files_per_piece_top-20.pdf', bbox_inches='tight')
+plt.show()
+
 
 # number of files per instrument group
 df_metadata.groupby('instrument_group').size().sort_values(ascending=True).plot.barh()
+plt.title('Number of Files per Instrument Group')
 plt.show()
 
 # number of titles per instrument group
-stats_titles = (~df_metadata.groupby(['instrument_group', 'title']).size().unstack(0).isna()).sum()
-stats_titles.sort_values(ascending=True).plot.barh()
+df_plot = (~df_metadata.groupby(['instrument_group', 'title']).size().unstack(0).isna()).sum().sort_values(ascending=True)
+sns.set_color_codes('muted')
+sns.barplot(y=df_plot.index, x=df_plot.values, color='b')
+plt.xlabel('#Musical Works')
+plt.ylabel('Instrument Group')
+sns.despine(left=True, bottom=True)
+plt.savefig('number_of_files_per_instrument_group.pdf', bbox_inches='tight')
 plt.show()
 
 # number of files per instrument
 df_metadata.groupby('instrument_from_fn').size().sort_values(ascending=True).plot.barh(figsize=(10, 30), fontsize=4)
+plt.show()
+
+# number of files per instrument (Top 20)
+df_plot = df_metadata.groupby('instrument_from_fn').size().sort_values(ascending=True).tail(20)
+sns.set_color_codes('muted')
+sns.barplot(y=df_plot.index, x=df_plot.values, color='b')
+plt.xlabel('#Files')
+plt.ylabel('Instrument')
+sns.despine(left=True, bottom=True)
+plt.savefig('number_of_files_per_instrument_top-20.pdf', bbox_inches='tight')
 plt.show()
 
 # number of pages
@@ -93,5 +130,3 @@ plt.show()
 
 
 # %%
-df_metadata.to_csv('metadata.csv')
-
